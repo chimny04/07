db7445d0e4e941d298903a6992503c39091c20f3
chimny04 <lucia.roback@gmail.com>
2023-10-19 15:19:51 +0900

    7-1

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int flag = 1;
+    int y;
+    while(flag != 0)
+    {
+        // int y;
+        y =3;
+        flag = 0;
+    }
+    y = 4; // y is not defined in original code
+  
+    system("PAUSE");	
+    return 0;
+}
